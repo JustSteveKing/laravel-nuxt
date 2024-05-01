a51061edee400f6cd589b74b79d7a9f28956d7a9
Steve McDougall <juststevemcd@gmail.com>
2024-05-01 19:22:13 +0100

    Setting NuxtJS up

--- web/app.vue
+++ web/app.vue
@@ -1,13 +1,17 @@
 <template>
-  <UContainer>
-    <UCard class="mt-10">
-      <template #header>
-        <div class="flex justify-between">
-          <h1>Welcome to Nuxt UI Starter</h1>
-          <ColorScheme><USelect v-model="$colorMode.preference" :options="['system', 'light', 'dark']" /></ColorScheme>
-        </div>
-      </template>
-      <UButton icon="i-heroicons-book-open" to="https://ui.nuxt.com" target="_blank">Open Nuxt UI Documentation</UButton>
-    </UCard>
-  </UContainer>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
+
+<style>
+.page-enter-active,
+.page-leave-active {
+  transition: all 0.4s;
+}
+.page-enter-from,
+.page-leave-to {
+  opacity: 0;
+  filter: blur(1rem);
+}
+</style>
--- web/nuxt.config.ts
+++ web/nuxt.config.ts
@@ -1,6 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    layoutTransition: {
+      name: 'layout',
+      mode: 'out-in'
+    },
+    pageTransition: {
+      name: 'page',
+      mode: 'out-in'
+    }
+  },
   devtools: { enabled: true },
+  experimental: {
+    viewTransition: true
+  },
   modules: ["@nuxt/ui", "nuxt-auth-sanctum"],
   sanctum: {
     baseUrl: 'http://localhost:8000',
